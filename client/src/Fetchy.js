1c3ab178b18660e35b27c1a9b9a542e2e61b2f12
Ferenc Ónodi <onodiferenc03@gmail.com>
2020-05-23 13:09:36 +0200

    feat: 🎸 Add Fetch funcionality

---
+++ client/src/Fetchy.js
@@ -0,0 +1,21 @@
+class Fetchy{
+static async Get (Link) {
+try{
+ const Response = await window.fetch()
+if (!Response.ok){
+throw Response.status
+}
+const data =await Response.json()
+return data}
+catch (error){
+    console.log('OOPS! Error requesting the requests that were requested with the requests :(')
+return error
+
+}
+
+}
+}
+
+
+
+
